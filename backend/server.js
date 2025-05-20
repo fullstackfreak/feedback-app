2f3f0160a2cc8cf59f99ac1b825fbb7cdb4628f9
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-05-20 11:30:06 +0530

    changed the cors

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
-const feedbackRoute = require("./routes/feedbackRoute")
+const feedbackRoute = require("./routes/feedbackRoute");
 
 const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localost:3000",
+    origin: "feedback-app-eight-iota.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
@@ -22,14 +22,11 @@ mongoose
   })
   .catch((err) => console.error(err));
 
-app.use("/api/feedback", feedbackRoute)
-
+app.use("/api/feedback", feedbackRoute);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
