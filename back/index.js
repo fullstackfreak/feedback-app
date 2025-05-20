58cdec23b32d1f89831df012c26c703b2da5fb52
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-05-20 12:50:40 +0530

    asdfass

--- back/index.js
+++ back/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const feedbackRoute = require("./routes/feedbackRoute");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
